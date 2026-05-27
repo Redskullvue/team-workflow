8a971392d2262471c17105c599552a6bdef3e234
“Redskullvue” <pouya.9996@gmail.com>
2026-05-27 14:47:14 +0330

    TailwindCSS/Vite Added to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  $production: {
+    devtools: { enabled: false },
+  },
+  $development: {
+    devtools: { enabled: true },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
+});
