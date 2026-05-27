403228ebef08f482559daf0a895f9a126a1d8a67
“Redskullvue” <pouya.9996@gmail.com>
2026-05-27 14:49:33 +0330

    Pinia Module Added to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+
   $production: {
     devtools: { enabled: false },
   },
+
   $development: {
     devtools: { enabled: true },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   css: ["~/assets/css/main.css"],
+  modules: ["@pinia/nuxt"],
 });
