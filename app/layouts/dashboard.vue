1a0da336bad3a540cf28444d604ffa8a185dcb2d
“Redskullvue” <pouya.9996@gmail.com>
2026-06-14 19:30:56 +0330

    Style : DashboardSidebar is now responsive on mobiles

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="w-full h-screen max-h-screen bg-white flex">
-    <aside class="lg:w-1/6 bg-white border-r border-gray-200 h-screen p-3">
+  <div class="w-full h-screen max-h-screen bg-white flex flex-col lg:flex-row">
+    <!-- Header only on mobile version -->
+    <header
+      class="w-full p-3 lg:hidden border-b-2 border-gray-200 shadow-sm shadow-gray-300 flex items-center justify-between"
+    >
+      <h2 class="font-bold p-3">
+        TaskFlow <span class="text-blue-500 text-sm">Dashboard</span>
+      </h2>
+      <button @click="toggle">
+        <Icon
+          name="ic:baseline-format-list-bulleted"
+          size="22px"
+          class="mt-2 text-blue-500"
+        />
+      </button>
+    </header>
+    <div
+      v-if="isOpen"
+      class="fixed inset-0 bg-black/50 z-20 lg:hidden"
+      @click="close"
+    />
+    <aside
+      class="lg:w-1/6 bg-white border-r border-gray-200 h-screen p-3 lg:block lg:translate-x-0 w-64 z-30 transition-transform duration-300 lg:static fixed"
+      :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
+    >
       <dashboard-side-bar />
     </aside>
     <slot />
   </div>
 </template>
 
-<script setup>
-import DashboardSideBar from "~/components/DashboardSideBar.vue";
+<script setup lang="ts">
+const { isOpen, toggle, close } = useSidebar();
 </script>
