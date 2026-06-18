4dcb6f4f7bd7dda62b88abd613272a0f7a6d25a4
“Redskullvue” <pouya.9996@gmail.com>
2026-06-18 23:19:59 +0330

    Feat : New Task button component

---
+++ app/components/AppNewTaskButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLink
+      class="w-full bg-blue-500 rounded-xl text-white flex items-center justify-center px-3 py-2 gap-x-1 cursor-pointer transition-colors duration-300 hover:bg-blue-600"
+    >
+      New Task
+      <span class="flex items-center"
+        ><Icon name="ic:twotone-plus" size="15px"
+      /></span>
+    </NuxtLink>
+  </div>
+</template>
--- app/components/DashboardSideBar.vue
+++ app/components/DashboardSideBar.vue
@@ -48,15 +48,7 @@
         Settings
       </NuxtLink>
       <UserProfile class="mb-2" />
-
-      <NuxtLink
-        class="w-full bg-blue-500 rounded-xl text-white flex items-center justify-center px-3 py-2 gap-x-1 cursor-pointer transition-colors duration-300 hover:bg-blue-600"
-      >
-        New Task
-        <span class="flex items-center"
-          ><Icon name="ic:twotone-plus" size="15px"
-        /></span>
-      </NuxtLink>
+      <AppNewTaskButton />
     </div>
   </div>
 </template>
